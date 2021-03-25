729629cd3edbe7ffb5714df024260afc69c2801d
Kevin Alves <kevinalves@hotmail.fr>
2021-03-25 16:42:56 +0100

    First commit

--- characters.py
+++ characters.py
@@ -8,9 +8,13 @@ class Characters:
     def __init__(self, type) :
         if type == "Perso1":
             self.type = "Perso1"
+            self.health = 100
         elif type == "Perso2":
-            self.type = "Perso2" 
+            self.type = "Perso2"
+            self.health = 100 
         elif type == "Perso3":
-            self.type = "Perso3" 
+            self.type = "Perso3"
+            self.health = 100 
         elif type == "Perso4":
-            self.type = "Perso4" 
+            self.type = "Perso4"
+            self.health = 100 
