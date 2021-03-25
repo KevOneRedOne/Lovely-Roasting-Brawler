ed124b9c4a3055492a32ad2123f216bb28deb953
Kevin Alves <kevinalves@hotmail.fr>
2021-03-25 16:32:25 +0100

    First commit

---
+++ characters.py
@@ -0,0 +1,16 @@
+'''
+Creation d'une classe regroupant tous les personnages jouables par les joueurs
+'''
+class Characters:
+    def __str___(self):
+        return "Le personnage choisi est " + self.type
+
+    def __init__(self, type) :
+        if type == "Perso1":
+            self.type = "Perso1"
+        elif type == "Perso2":
+            self.type = "Perso2" 
+        elif type == "Perso3":
+            self.type = "Perso3" 
+        elif type == "Perso4":
+            self.type = "Perso4" 
