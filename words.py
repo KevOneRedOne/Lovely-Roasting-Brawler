6ab1dd2893aae07698e3f6f05648a4dfbecb23d2
DJOUDI Yanis <65081645+Tadayoshi123@users.noreply.github.com>
2021-03-29 12:56:56 +0200

    Prototype Mots

--- words.py
+++ words.py
@@ -3,4 +3,35 @@
 Creation d'une classe regroupant tous les mots disponibles ! 
 
 '''
+class Sujet:
+    def __init__(self):
+        self.liste = [
+            "Mon chien","Votre mère","Mes chaussettes"
+        ]
 
+            
+
+
+class Verbe:
+    def __init__(self):
+        self.liste= [
+            "Est parti","A lavé","était","sont"
+        ]
+
+class Complement:
+    def __init__(self):
+        self.liste= [
+            "Ce soir","à l'école","La semaine dernière"
+        ]
+
+class Liaisons:
+    def __init__(self):
+        self.liste = [
+            "et","car","en d'autres termes","par ailleurs","sans compter que"
+        ]
+
+class FinDePhrase:
+    def __init__(self):
+        self.liste = [
+            "Et tout le monde le sait !","Et bim !","Tu peux pas le nier"
+        ]
