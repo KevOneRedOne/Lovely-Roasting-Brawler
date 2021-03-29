4e200c9fc1ce115b4ab14a70aea04cfd350443c3
Kevin Alves <kevinalves@hotmail.fr>
2021-03-29 12:19:22 +0200

    MAJ Projet

--- characters.py
+++ characters.py
@@ -3,27 +3,32 @@ Creation d'une classe regroupant tous les personnages jouables par les joueurs
 '''
 class Characters:
     def __str___(self):
-        return "Le personnage s'appelle" + self.name + self.adjective + "et ... " + self.malus
+        return "Le personnage s'appelle" + self.name + self.adjective 
 
     def __init__(self, name) :
         if name == "Bryan":
             self.name = "Bryan"
             self.adjective = "Le gamin énervant !"
             self.health = 100
-            self.faiblesse = "Aime "
+            self.faiblesse = []
         elif name == "Francis":
             self.name = "Francis"
             self.adjective = "Le vieux !"
             self.health = 100 
+            self.faiblesse = []
         elif name == "Jean-Michel":
             self.name = "Jean-Michel"
             self.adjective = "Le grincheux susceptible !"
             self.health = 100 
+            self.faiblesse = []
         elif name == "Martha":
             self.name = "Martha"
             self.adjective = "Championne UFC"
             self.health = 100 
+            self.faiblesse = []
         elif name == "Agnès":
             self.name = "Agnès"
             self.adjective = "La naïve"
             self.health = 100 
+            self.faiblesse = []
+
